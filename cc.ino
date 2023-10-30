024094a0f3c243d166cb25d0beb190a4143deb64
MURALIKRISHNA S <murxli.krishna@gmail.com>
2023-10-30 09:26:43 +0530

    Add files via upload

--- cc.ino
+++ cc.ino
@@ -1,10 +1,13 @@
 #include "config.h"
 
+#include "WiFi.h"
 #include <WiFiClientSecure.h>
 #include <MQTTClient.h> //MQTT Library Source: https://github.com/256dpi/arduino-mqtt
-
 #include <ArduinoJson.h> //ArduinoJson Library Source: https://github.com/bblanchon/ArduinoJson
-#include "WiFi.h"
+#include <DHT.h>
+#include <Wire.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 
 // MQTT topics for the device
 #define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
@@ -13,27 +16,43 @@
 WiFiClientSecure wifi_client = WiFiClientSecure();
 MQTTClient mqtt_client = MQTTClient(256); //256 indicates the maximum size for packets being published and received.
 
-uint32_t temperature;
+#define DHTPIN 2     
+#define DHTTYPE DHT22  
+
+DHT dht(DHTPIN, DHTTYPE);
+Adafruit_MPU6050 mpu;
+
+struct Vibration{
+  float acceleration[3];
+  float rotation[3];
+};
+
+float temperature;
+float humidity;
+Vibration vibration;
+
 uint32_t current;
 uint32_t voltage;
 uint32_t Speed;
-uint32_t Vibration;
+// uint32_t Vibration;
 
-void connectAWS()
-{
-  //Begin WiFi in station mode
+int tempcount = 0;
+
+void connectToWifi(){
+    //Begin WiFi in station mode
   WiFi.mode(WIFI_STA); 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
   Serial.println("Connecting to Wi-Fi");
-
   //Wait for WiFi connection
   while (WiFi.status() != WL_CONNECTED){
     delay(500);
     Serial.print(".");
   }
   Serial.println("Wifi connected");
+}
 
+void connectAWS()
+{
   // Configure wifi_client with the correct certificates and keys
   wifi_client.setCACert(AWS_CERT_CA);
   wifi_client.setCertificate(AWS_CERT_CRT);
@@ -69,15 +88,26 @@ void publishMessage()
 {
   //Create a JSON document of size 200 bytes, and populate it
   //See https://arduinojson.org/
-  StaticJsonDocument<200> doc;
+  StaticJsonDocument<400> doc;
   // doc["elapsed_time"] = millis() - t1;
   // doc["value"] = random(1000);
+  doc["temperature"] = temperature;
+  doc["humidity"] = humidity;
+  doc["Vibration"]["acceleration"][0] = vibration.acceleration[0];
+  doc["Vibration"]["acceleration"][1] = vibration.acceleration[1];
+  doc["Vibration"]["acceleration"][2] = vibration.acceleration[2];
+  doc["Vibration"]["rotation"][0] = vibration.rotation[0];
+  doc["Vibration"]["rotation"][1] = vibration.rotation[1];
+  doc["Vibration"]["rotation"][2] = vibration.rotation[2];
+
+
+  // doc["Vibration"]["rotation"] = vibration.rotation;
+
+
   doc["timeStamp"] = time(NULL);
   doc["turbine speed"] = random(1000);
-  doc["temperature"] = random(150);
   doc["current"] = random(30);
   doc["voltage"] = random(30);
-  doc["Vibration"] = random(40);
 
   char jsonBuffer[512];
   serializeJson(doc, jsonBuffer); // print to mqtt_client
@@ -93,13 +123,142 @@ void incomingMessageHandler(String &topic, String &payload) {
   Serial.println("Payload: " + payload);
 }
 
+int readDHT(){
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return -1;
+  }
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print("%  Temperature: ");
+  Serial.print(temperature);
+  Serial.println(F("°C "));
+  return 0;
+}
+
+void setupMPU(){
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+
+  Serial.println("");
+  delay(100);
+}
+
+void readMPU(){
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  Serial.print("Rotation X: ");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  Serial.println("");
+
+  vibration.acceleration[0] = a.acceleration.x;
+  vibration.acceleration[1] = a.acceleration.y;
+  vibration.acceleration[2] = a.acceleration.z;
+
+  vibration.rotation[0] = g.gyro.x;
+  vibration.rotation[1] = g.gyro.y;
+  vibration.rotation[2] = g.gyro.z;
+
+}
 void setup() {
   Serial.begin(115200);
-  // t1 = millis();
+
+  dht.begin();
+  Wire.begin();
+
+  setupMPU();
+  connectToWifi();
   connectAWS();
 }
 
 void loop() {
+  readDHT();
+  readMPU();
   publishMessage();
   mqtt_client.loop();
   delay(10000);
